1bb573509008884d398e4b5684198bfe0bea5daa
Yangseungyun <supersatori@naver.com>
2023-07-04 17:36:13 +0900

    [추가] 검색결과 여러개 나타내기

--- Hospital ERP/src/main/java/hospital/action/Action.java
+++ Hospital ERP/src/main/java/hospital/action/Action.java
@@ -9,5 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 
 public interface Action {
 	public ActionForward execute(HttpServletRequest request , HttpServletResponse response);
+	
 }
 
--- Hospital ERP/src/main/java/hospital/action/ActionForward.java
+++ Hospital ERP/src/main/java/hospital/action/ActionForward.java
@@ -1,13 +1,5 @@
 package hospital.action;
 
-/*
-servelt(front) 요청 받아요
-1. 화면보여주세요
-2. 로직 처리해 주세요  
-  
-화면 아니면 로직  
-*/
-
 public class ActionForward {
 	private boolean isRedirect=false; //뷰의 전환 여부  ...redirect or  forward
 	private String path=null; //이동 경로 (뷰의 주소)
