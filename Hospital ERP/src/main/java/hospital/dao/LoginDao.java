165134e5f43a4073c5a900a4d22099cde93b43eb
Jung <soft8991@naver.com>
2023-07-05 17:25:06 +0900

    [추가] SignUp 생성

--- Hospital ERP/src/main/java/hospital/dao/LoginDao.java
+++ Hospital ERP/src/main/java/hospital/dao/LoginDao.java
@@ -6,21 +6,20 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import hospital.utils.ConnectionHelper;
-import hospital.vo.LoginVO;
 
 public class LoginDao {
 
-	public int loginCheck(String id, String pwd) {
+	public int loginCheck(String h_id, String h_pwd) {
 		int ret = -1;
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
 			conn = ConnectionHelper.getConnection();
-			String sql = "SELECT COUNT(*) FROM LOGIN WHERE ID = ? AND PWD = ?";
+			String sql = "SELECT COUNT(*) FROM HOSPITAL_ID WHERE H_ID = ? AND H_PWD = ?";
 			pstmt = conn.prepareStatement(sql);
-			pstmt.setString(1, id);
-			pstmt.setString(2, pwd);
+			pstmt.setString(1, h_id);
+			pstmt.setString(2, h_pwd);
 
 			rs = pstmt.executeQuery();
 			rs.next();
@@ -34,33 +33,5 @@ public class LoginDao {
 			ConnectionHelper.close(conn);
 		}
 		return ret;
-	}
-
-	public int login(LoginVO vo) {
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		int resultrow = 0;
-		try {
-			conn = ConnectionHelper.getConnection();
-			String sql = "INSERT INTO login(id,pwd,h_name,email,phone,post,address) VALUES(?,?,?,?,?,?,?)";
-			pstmt = conn.prepareStatement(sql);
-
-			pstmt.setString(1, vo.getId());
-			pstmt.setString(2, vo.getPwd());
-			pstmt.setString(3, vo.getH_name());
-			pstmt.setString(4, vo.getEmail());
-			pstmt.setString(5, vo.getPhone());
-			pstmt.setString(6, vo.getPost());
-			pstmt.setString(7, vo.getAddress());
-
-			resultrow = pstmt.executeUpdate();
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			ConnectionHelper.close(pstmt);
-			ConnectionHelper.close(conn);
-		}
-		return resultrow;
 	}
 }
--- Hospital ERP/src/main/java/hospital/vo/LoginVO.java
+++ Hospital ERP/src/main/java/hospital/vo/LoginVO.java
@@ -1,67 +1,78 @@
 package hospital.vo;
 
+// CREATE TABLE hospital_id ( --병원아이디
+//		h_id	varchar2(20)		NOT NULL, --병원ID
+//		h_name	varchar2(50)		NULL, --병원명
+//		h_pwd	varchar2(30)		NOT NULL, --병원 비밀번호
+//		h_phone	varchar2(20)		NULL, --병원 전화번호
+//      h_email varchar2(40)        NOT NULL, --병원 이메일
+//		h_post	varchar2(10)		NULL --병원 우편번호
+//		h_address	varchar2(200)		NULL, --병원주소
+//	);
+
+
 //로그인 정보
 public class LoginVO {
 
-	private String id, pwd, h_name, email, phone, post, address;
+	private String h_id, h_name, h_address, h_phone, h_pwd, h_email, h_post;
 
-	public LoginVO() {
+	public LoginVO() {	}
+
+	public String getH_id() {
+		return h_id;
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getPwd() {
-		return pwd;
-	}
-
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
+	public void setH_id(String h_id) {
+		this.h_id = h_id;
 	}
 
 	public String getH_name() {
 		return h_name;
 	}
 
+	public String getH_email() {
+		return h_email;
+	}
+
+	public void setH_email(String h_email) {
+		this.h_email = h_email;
+	}
+
 	public void setH_name(String h_name) {
 		this.h_name = h_name;
 	}
 
-	public String getEmail() {
-		return email;
+	public String getH_pwd() {
+		return h_pwd;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	public void setH_pwd(String h_pwd) {
+		this.h_pwd = h_pwd;
 	}
 
-	public String getPhone() {
-		return phone;
+	public String getH_phone() {
+		return h_phone;
 	}
 
-	public void setPhone(String phone) {
-		this.phone = phone;
+	public void setH_phone(String h_phone) {
+		this.h_phone = h_phone;
 	}
 
-	public String getPost() {
-		return post;
+	public String getH_post() {
+		return h_post;
 	}
 
-	public void setPost(String post) {
-		this.post = post;
+	public void setH_post(String h_post) {
+		this.h_post = h_post;
 	}
 
-	public String getAddress() {
-		return address;
+	public String getH_address() {
+		return h_address;
 	}
 
-	public void setAddress(String address) {
-		this.address = address;
+	public void setH_address(String h_address) {
+		this.h_address = h_address;
 	}
 
+	
 }
