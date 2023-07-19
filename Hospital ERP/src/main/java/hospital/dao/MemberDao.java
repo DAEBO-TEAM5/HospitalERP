447e958a736e1eea3446abc618a34f5082d20074
Jung <soft8991@naver.com>
2023-07-20 08:42:52 +0900

    [수정]헤더 ,중복코드

--- Hospital ERP/src/main/java/hospital/dao/MemberDao.java
+++ Hospital ERP/src/main/java/hospital/dao/MemberDao.java
@@ -3,14 +3,86 @@ package hospital.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
 import hospital.utils.ConnectionHelper;
+import hospital.vo.LoginVO;
 
 public class MemberDao {
+
+	public static MemberDao instance;
+	Connection conn;
+	PreparedStatement pstmt;
+	ResultSet rs;
+
+	public MemberDao() {
+		
+	}
+
+	public LoginVO selectMember(String h_id, String h_email) {
+
+		String sql = "SELECT H_ID, H_EMAIL FROM HOSPITAL_ID WHERE H_ID=? AND H_EMAIL=?";
+		LoginVO vo = null;
+
+		try {
+			conn = ConnectionHelper.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, h_id);
+			pstmt.setString(2, h_email);
+			rs = pstmt.executeQuery();
+
+			if (rs.next()) {
+				vo = new LoginVO();
+				vo.setH_id(h_id);
+				vo.setH_email(h_email);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			ConnectionHelper.close(rs);
+			ConnectionHelper.close(pstmt);
+			ConnectionHelper.close(conn);
+		}
+		
+		return vo;
+	}
+
+	public int updatePw(String h_id, String newPw) {
+
+		int updateCount = 0;
+		String sql = "UPDATE HOSPITAL_ID " + "SET H_PWD=? " + "WHERE H_ID=?";
+
+		try {
+			conn = ConnectionHelper.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, newPw);
+			pstmt.setString(2, h_id);
+			updateCount = pstmt.executeUpdate();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			ConnectionHelper.close(conn);
+			ConnectionHelper.close(pstmt);
+		}
+
+		return updateCount;
+	}
+
+	public LoginVO getMember(String h_id, String h_email) {
+
+		conn = ConnectionHelper.getConnection();
+		MemberDao dao = new MemberDao();
+		LoginVO vo = dao.selectMember(h_id, h_email);
+
+		ConnectionHelper.close(conn);
+		return vo;
+	}
+	
 	public boolean duplicateIdCheck(String h_id) {
 
-		Connection conn = null;
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		
 		boolean isDuplicate = false;
 
 		try {
--- Hospital ERP/src/main/java/hospital/service/MemberForgotPwProAction.java
+++ Hospital ERP/src/main/java/hospital/service/MemberForgotPwProAction.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpSession;
 
 import hospital.action.Action;
 import hospital.action.ActionForward;
-import hospital.dao.Member2Dao;
+import hospital.dao.MemberDao;
 import hospital.utils.NaverMailSend;
 import hospital.vo.LoginVO;
 
@@ -31,20 +31,22 @@ public class MemberForgotPwProAction implements Action {
 			PrintWriter out;
 			try {
 				out = resp.getWriter();
-				out.println("<script>" + "alert('접근 권한이 없습니다.1');" + "location.href='/login.do';" + "</script>");
+				out.println("<script>" + "alert('접근 권한이 없습니다.');" + "location.href='/login.do';" + "</script>");
 				out.flush();
 				out.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			return null;
+
 		} else {
-			
+
 			String h_email = req.getParameter("h_email");
 			String h_id = req.getParameter("h_id");
 
-			Member2Dao dao = new Member2Dao();
+			MemberDao dao = new MemberDao();
 			LoginVO vo = (LoginVO) dao.getMember(h_id, h_email);
+			
 			if (vo == null || !vo.getH_id().equals(h_id) || !vo.getH_email().equals(h_email)) {
 				try {
 					resp.sendError(404);
@@ -52,14 +54,13 @@ public class MemberForgotPwProAction implements Action {
 					e.printStackTrace();
 				}
 			} else {
-				
+
 				NaverMailSend mailSend = new NaverMailSend();
 
 				try {
 					String authenticationCode = mailSend.sendEmail(h_email);
 					session.setAttribute("authCode", authenticationCode);
 					session.setAttribute("id", h_id);
-					
 
 					forward = new ActionForward();
 					forward.setPath("./memberChangePw.do");
